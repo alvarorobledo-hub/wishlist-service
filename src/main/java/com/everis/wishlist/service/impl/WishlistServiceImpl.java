dc1c624f8bbf93a590cb4b4180c4b2ad19439970
alvaro robledo <alvaro.robledo@sportium.com>
2023-10-26 17:48:33 +0200

    feat: create user wishlist repository

--- src/main/java/com/everis/wishlist/service/impl/WishlistServiceImpl.java
+++ src/main/java/com/everis/wishlist/service/impl/WishlistServiceImpl.java
@@ -2,6 +2,7 @@ package com.everis.wishlist.service.impl;
 
 import com.everis.wishlist.client.ProductApiClient;
 import com.everis.wishlist.dto.response.UserWishlistResponse;
+import com.everis.wishlist.repository.UserWishlistRepository;
 import com.everis.wishlist.service.WishlistService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,7 @@ public class WishlistServiceImpl implements WishlistService {
     private final UserWishlistRepository userWishlistRepository;
 
     @Override
-    public UserWishlistResponse findUserWishlist(UUID userId, UUID wishlistId) {
+    public UserWishlistResponse findUserWishlist(final UUID userId, final UUID wishlistId) {
         return null;
     }
 }
