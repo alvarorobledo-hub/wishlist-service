2cad54aabab8a4d56d454e35312a0e1e237f52fa
alvaro robledo <alvaro.robledo@sportium.com>
2023-10-26 17:29:29 +0200

    feat: create feign configuration

--- src/main/java/com/everis/wishlist/WishlistServiceApplication.java
+++ src/main/java/com/everis/wishlist/WishlistServiceApplication.java
@@ -2,7 +2,9 @@ package com.everis.wishlist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class WishlistServiceApplication {
 
