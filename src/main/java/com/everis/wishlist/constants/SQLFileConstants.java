98aa7d361e737198f518f96da5e8cabc759ad908
alvaro robledo <alvaro.robledo@sportium.com>
2023-10-26 20:16:20 +0200

    feat: add constants

--- src/main/java/com/everis/wishlist/constants/SQLFileConstants.java
+++ src/main/java/com/everis/wishlist/constants/SQLFileConstants.java
@@ -2,5 +2,6 @@ package com.everis.wishlist.constants;
 
 public class SQLFileConstants {
 
-    public static final String FILE_FIND_USER_LIST = "/db/files/find_user_list.sql";
+    public static final String FILE_FIND_USER_WISHLIST = "/db/files/find_user_wishlist.sql";
+    public static final String FILE_FIND_USER_WISHLISTS = "/db/files/find_user_wishlists.sql";
 }
