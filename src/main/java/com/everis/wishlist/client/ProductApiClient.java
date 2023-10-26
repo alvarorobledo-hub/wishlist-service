f4ffea9d80d693972f9404f9c54e09ab6686ba54
alvaro robledo <alvaro.robledo@sportium.com>
2023-10-26 17:30:27 +0200

    feat: create product api client

---
+++ src/main/java/com/everis/wishlist/client/ProductApiClient.java
@@ -0,0 +1,12 @@
+package com.everis.wishlist.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "product-api-client", url = "${urls.product-api-client}")
+public interface ProductApiClient {
+
+    @GetMapping(value = "/product/{productId}")
+    String getProduct(@PathVariable String productId);
+}
