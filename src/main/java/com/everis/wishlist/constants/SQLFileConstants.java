0c1e6798c817486fe8473d89580bcf1cc1eff529
alvaro robledo <alvaro.robledo@sportium.com>
2023-10-26 21:01:02 +0200

    feat: add files for create wishlist

--- src/main/java/com/everis/wishlist/constants/SQLFileConstants.java
+++ src/main/java/com/everis/wishlist/constants/SQLFileConstants.java
@@ -2,6 +2,10 @@ package com.everis.wishlist.constants;
 
 public class SQLFileConstants {
 
+    public static final String FILE_CREATE_USER_WISHLIST = "/db/files/create_user_wishlist.sql";
+    public static final String FILE_CREATE_WISHLIST = "/db/files/create_wishlist.sql";
+    public static final String FILE_CREATE_WISHLIST_PRODUCT = "/db/files/create_wishlist_product.sql";
+
     public static final String FILE_FIND_USER_WISHLIST = "/db/files/find_user_wishlist.sql";
     public static final String FILE_FIND_USER_WISHLISTS = "/db/files/find_user_wishlists.sql";
 }
---
+++ src/main/resources/db/files/create_wishlist_product.sql
@@ -0,0 +1,1 @@
+INSERT INTO WISHLIST_PRODUCT (wishlist_id, product_id) VALUES (:wishlist_id, :product_id)
