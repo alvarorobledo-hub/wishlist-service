6b60ffef0f700a5b954ece56c01fc1207360b478
alvaro robledo <alvaro.robledo@sportium.com>
2023-10-26 17:49:27 +0200

    feat: create user wishlist repository

--- src/main/java/com/everis/wishlist/repository/impl/UserWishlistRepositoryImpl.java
+++ src/main/java/com/everis/wishlist/repository/impl/UserWishlistRepositoryImpl.java
@@ -2,10 +2,17 @@ package com.everis.wishlist.repository.impl;
 
 import com.everis.wishlist.entity.Wishlist;
 import com.everis.wishlist.repository.UserWishlistRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
+@RequiredArgsConstructor
 public class UserWishlistRepositoryImpl implements UserWishlistRepository {
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public Wishlist findUserWishList(final UUID userId, final UUID wishlistId) {
