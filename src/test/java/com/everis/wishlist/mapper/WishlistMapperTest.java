44854588c9dbbe2fe42d26d9cf751d9089e0b56e
alvaro robledo <alvaro.robledo@sportium.com>
2023-10-27 18:12:13 +0200

    feat: add mapper test

--- src/test/java/com/everis/wishlist/mapper/WishlistMapperTest.java
+++ src/test/java/com/everis/wishlist/mapper/WishlistMapperTest.java
@@ -3,20 +3,28 @@ package com.everis.wishlist.mapper;
 import com.everis.wishlist.client.ProductApiClient;
 import com.everis.wishlist.entity.Wishlist;
 import com.everis.wishlist.entity.WishlistDetail;
+import com.everis.wishlist.repository.UserWishlistRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import feign.FeignException;
+import feign.Request;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static com.everis.wishlist.constants.WishlistServiceConstants.PRODUCT_ID;
+import static com.everis.wishlist.constants.WishlistServiceConstants.WISHLIST_ID;
 import static com.everis.wishlist.mock.WishlistServiceMock.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class WishlistMapperTest {
 
+    @Mock
+    private UserWishlistRepository userWishlistRepository;
     @Mock
     private ProductApiClient productApiClient;
 
@@ -40,6 +48,29 @@ class WishlistMapperTest {
         // THEN
         assertAll("Mapper should be:",
                 () -> assertEquals(wishlistDetail, response));
+        verify(userWishlistRepository, never()).deleteUserWishlistProduct(WISHLIST_ID, PRODUCT_ID);
+    }
+
+    @Test
+    void should_delete_a_product_and_return_a_wishlist_without_it() throws JsonProcessingException {
+        final Wishlist wishlist = getWishlist();
+        final WishlistDetail wishlistDetail = getWishlistDetail();
+        FeignException.NotFound notFoundException = Mockito.mock(FeignException.NotFound.class);
+
+        // GIVEN
+        wishlist.getProductIds().add(22L);
+        doReturn(getStringProduct(4)).when(productApiClient).getProduct(4L);
+        doReturn(getStringProduct(5)).when(productApiClient).getProduct(5L);
+        doReturn(getStringProduct(6)).when(productApiClient).getProduct(6L);
+        doThrow(notFoundException).when(productApiClient).getProduct(22L);
+
+        // WHEN
+        final WishlistDetail response = wishlistMapper.from(wishlist);
+
+        // THEN
+        assertAll("Mapper should be:",
+                () -> assertEquals(wishlistDetail, response));
+        verify(userWishlistRepository).deleteUserWishlistProduct(WISHLIST_ID, 22L);
     }
 
 }
